1999c634c9b828edfe1461ff6179311832d0857b
agajo <madaopc@gmail.com>
2020-11-11 12:14:22 +0900

    scaled example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,53 +15,55 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-            child: Column(
-          children: [
-            DecoratedText(
-              'Decorated Text',
-              fontSize: 40,
-              fontWeight: FontWeight.w600,
-              fillColor: Colors.teal,
-              borderWidth: 1,
-            ),
-            DecoratedText(
-              'Decorated Text',
-              borderColor: Colors.amber,
-              borderWidth: 3,
-              fontSize: 40,
-              fontWeight: FontWeight.w800,
-              shadows: [
-                Shadow(color: Colors.black, blurRadius: 4, offset: Offset(4, 4))
-              ],
-              fillGradient: LinearGradient(colors: [Colors.blue, Colors.red]),
-            ),
-            DecoratedGoogleFontText(
-              'Decorated Google Font',
-              fontMethod: GoogleFonts.rancho,
-              fontSize: 40,
-              fontWeight: FontWeight.w800,
-              borderWidth: 1.5,
-              borderColor: Colors.yellow[800],
-              shadows: const [
-                Shadow(color: Colors.black, blurRadius: 4, offset: Offset(4, 4))
-              ],
-              fillGradient: LinearGradient(
+            child: Transform.scale(
+          scale: 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DecoratedText(
+                'Decorated Text',
+                fontSize: 40,
+                fontWeight: FontWeight.w600,
+                fillColor: Colors.teal,
+                borderWidth: 1,
+              ),
+              DecoratedText(
+                'Decorated Text',
+                borderColor: Colors.amber,
+                borderWidth: 3,
+                fontSize: 40,
+                fontWeight: FontWeight.w800,
+                shadows: [
+                  Shadow(
+                      color: Colors.black, blurRadius: 4, offset: Offset(4, 4))
+                ],
+                fillGradient: LinearGradient(colors: [Colors.blue, Colors.red]),
+              ),
+              DecoratedGoogleFontText(
+                'Decorated Google Font',
+                fontMethod: GoogleFonts.rancho,
+                fontSize: 40,
+                fontWeight: FontWeight.w800,
+                borderWidth: 1.5,
+                borderColor: Colors.yellow[800],
+                shadows: const [
+                  Shadow(
+                      color: Colors.black, blurRadius: 4, offset: Offset(4, 4))
+                ],
+                fillGradient: LinearGradient(
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
-                  stops: const [
-                    0.2,
-                    0.55,
-                    0.55,
-                    0.75
-                  ],
+                  stops: const [0.2, 0.55, 0.55, 0.75],
                   colors: [
                     Colors.white,
                     Colors.yellow[500],
                     Colors.yellow[800],
                     Colors.yellow[500]
-                  ]),
-            )
-          ],
+                  ],
+                ),
+              )
+            ],
+          ),
         )),
       ),
     );
